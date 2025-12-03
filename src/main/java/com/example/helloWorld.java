c3a0fa89bd8c172d02352f94514eab3cb1c147c9
Datasyncml <shyaanmd707@gmail.com>
2025-12-04 01:29:34 +0530

    Delete src/main/java/com/example directory

--- src/main/java/com/example/helloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-public class helloworld {
-public static void main(String[] args) {
-  System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
-  System.out.println("Change #1: Verifying automatic workflow trigger on push.");
-}
-}
